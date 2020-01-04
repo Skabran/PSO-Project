b4bb983aa595d3d51ad4e6ca37f88a210dc40314
DreamYourLegend <58189919+DreamYourLegend@users.noreply.github.com>
2020-01-04 13:26:06 +0100

    Update setUpParams.cpp

--- setUpParams.cpp
+++ setUpParams.cpp
@@ -24,14 +24,17 @@ void SetUpParams::independent_runs(const unsigned int val)
 {
     _independent_runs = val;
 }
+
 void SetUpParams::nb_evolution_steps(const unsigned int val)
 {
     _nb_evolution_steps = val;
 }
+
 void SetUpParams::population_size(const unsigned int val)
 {
     _population_size = val;
 }
+
 void SetUpParams::solution_size(const unsigned int val)
 {
     _solution_size = val;
